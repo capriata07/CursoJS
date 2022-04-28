5310a072ba5c477ed9251b8e10c546e806a33be7
capriata07 <rafael.capriata07@gmail.com>
2022-04-28 12:08:53 -0400

    objetos no java script

---
+++ 02-03-objetos/script.js
@@ -0,0 +1,15 @@
+let person = {
+  name : 'Rafael',
+  age : 37,
+  sex : 'male'
+
+}
+
+// neste codigo eu re atribui o valor do atributo idade pra 40
+person.age = 40
+
+person.name = 'Juliano'
+
+console.log(person)
+
+console.log(person.age)
