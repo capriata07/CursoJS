a567485e890d79c4537208410a70e0afb0c0cdb4
capriata07 <rafael.capriata07@gmail.com>
2022-04-28 17:42:57 -0400

    funcoes-javascript

---
+++ 02-05-funcoes/script.js
@@ -0,0 +1,14 @@
+function greet (name) {
+  return 'Boa noite ' + name
+}
+
+let greeting = greet('Rafael')
+console.log(greeting)
+
+
+/*
+se eu so chamar a funcao nada acontece
+por causa do return 
+*/
+
+greet('Felipe')
